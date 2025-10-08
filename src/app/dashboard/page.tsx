7ee0a66ea529ea127d2051164b60a536175e0da6
Amoga devi <amogadevi.s10@gmail.com>
2025-10-08 06:52:03 +0000

    recent user box mak it grey colour (_for element div_)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -81,7 +81,7 @@ export default async function DashboardPage() {
           description="From all loans"
         />
       </div>
-      <Card className="bg-notification-popover text-notification-popover-foreground">
+      <Card>
         <CardHeader className="flex flex-row items-center">
           <div className="grid gap-2">
             <CardTitle>Recent Users</CardTitle>
