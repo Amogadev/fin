0012bad6b91a32eb9643f7fbba1ea4c2acc77472
Amoga devi <amogadevi.s10@gmail.com>
2025-10-08 06:54:52 +0000

    make this 3 box reduce the square  (_for element div_)

--- src/components/stat-card.tsx
+++ src/components/stat-card.tsx
@@ -21,7 +21,7 @@ export default function StatCard({
   icon: Icon,
 }: StatCardProps) {
   return (
-    <Card className="aspect-square flex flex-col justify-center">
+    <Card className="flex flex-col justify-center h-40">
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-sm font-medium">{title}</CardTitle>
         <Icon className="h-4 w-4 text-muted-foreground" />
