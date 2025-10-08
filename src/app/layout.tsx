a4308d33b89f4679f951896267568a9714e5c582
Amoga devi <amogadevi.s10@gmail.com>
2025-10-08 08:25:02 +0000

    Vault 360 (_for element h1_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Vault360",
+  title: "Vault 360",
   description: "A Smart Finance / Loan Management System",
 };
 
